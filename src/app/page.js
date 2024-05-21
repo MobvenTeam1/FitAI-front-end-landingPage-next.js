d9dd2393afb124a2d5366f916bdad29186093497
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2024-05-21 13:27:01 +0300

    tempate added

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+// import Image from "next/image";
+{
+  /* <Image
+className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+src="/next.svg"
+alt="Next.js Logo"
+width={180}
+height={37}
+priority
+/> */
+}
+
+export default function Home() {
+  return <div className="text-red-500">Template</div>;
+}
