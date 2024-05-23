607955bad5a47c94be89cd4ce343d61fec987929
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2024-05-23 11:43:52 +0300

    Accordion design updated

--- src/sections/Questions/index.js
+++ src/sections/Questions/index.js
@@ -5,7 +5,7 @@ export default function Questions() {
     <div className="container mx-auto min-h-screen grid grid-cols-12 items-center gap-28 max-sm:gap-0 max-sm:px-4 max-sm:min-h-full max-sm:py-20">
       <div className="col-span-4 flex flex-col gap-6 max-sm:col-span-12 max-sm:gap-3">
         <h3 className="text-5xl font-bold max-sm:text-3xl">Sıkça Sorulan Sorular</h3>
-        <p className="text-2xl max-sm:text-lg">
+        <p className="text-xl max-sm:text-lg">
           Hala yardıma ihtiyacınız var mı?{"  "}
           <span className="font-bold text-christi underline">
             Şimdi Yardım Alın
