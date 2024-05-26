2bc1144bea7347679c658ae6a14e5dc9ebe4b6d9
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2024-05-26 20:53:06 +0300

    blog card responsive design added

--- src/sections/Blog/card.js
+++ src/sections/Blog/card.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const BlogCard = ({ img_url, date, title, description, tags }) => {
   return (
-    <div className="flex flex-col gap-6">
+    <div className="flex flex-col gap-6 max-sm:gap-4">
       <Image
         src={img_url}
         alt={img_url}
@@ -12,17 +12,19 @@ const BlogCard = ({ img_url, date, title, description, tags }) => {
         height={100}
         priority
       />
-      <div className="flex flex-col gap-4">
-        <p className="text-base font-normal text-blackCoral">
+      <div className="flex flex-col gap-4 max-sm:gap-2">
+        <p className="text-base max-sm:text-sm font-normal text-blackCoral">
           {date.toLocaleDateString()}
         </p>
-        <div className="font-bold mb-2 text-2xl">{title}</div>
-        <p className="text-blackCoral text-lg line-clamp-3">{description}</p>
-        <div className="flex items-center gap-2">
+        <div className="font-bold text-2xl max-sm:text-xl">{title}</div>
+        <p className="text-blackCoral text-lg line-clamp-3 max-sm:text-base">
+          {description}
+        </p>
+        <div className="flex items-center gap-2 max-sm:gap-1">
           {tags.map((tag, index) => (
             <p
               key={`${tag}+${index}`}
-              className="bg-cultured text-blackCoral rounded-full text-sm text-center py-2 px-4"
+              className="bg-cultured text-blackCoral rounded-full text-sm text-center max-sm:text-xs py-2 px-4 max-sm:px-3"
             >
               {tag}
             </p>
